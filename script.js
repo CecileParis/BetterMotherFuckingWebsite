158518374d39daae8a077674f8da92f335106450
Cecile Costomiris <cecile.costomiris@gmail.com>
2018-06-27 10:03:47 +0200

    Ajout de script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function popup () {
+  console.log(alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please'))
+}
+  setTimeout (popup, 10000)
+
+function myFunction() {
+  document.body.style.backgroundColor = "magenta";
+}
